330f76da7db78a5e379f5b2e7bb74d8b3a7fa19d
appeon <41135355+Appeon2019@users.noreply.github.com>
2019-05-28 11:20:47 +0800

    Update Startup.cs

--- Appeon.ModelStoreDemo.Oracle/Startup.cs
+++ Appeon.ModelStoreDemo.Oracle/Startup.cs
@@ -28,7 +28,7 @@ namespace Appeon.ModelStoreDemo.Oracle
 				m.UsePowerBuilderIntegrated();
             });
 
-           // Note: Replace "ContextName" with the configuration context name; replace "key" with the database connection name that exists in appsettings.json. The sample code is as follows:
+	    //Note: Change "OrderContext" if you have changed the default DataContext file name; change the "AdventureWorks" if you have changed the database connection name in appsettings.json            
             services.AddDataContext<OrderContext>(m => m.UseOracle(Configuration["ConnectionStrings:AdventureWorks"]));
 
             services.AddScoped<IProductService, ProductService>();
